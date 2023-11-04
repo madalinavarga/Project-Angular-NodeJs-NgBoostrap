910add7736b8e69146a56717a97d78f541ab9d4e
madalinavarga <79206952+madalinavarga@users.noreply.github.com>
2023-11-04 12:47:15 +0200

    form

--- Client/src/app/contact/contact/contact.component.ts
+++ Client/src/app/contact/contact/contact.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, RequiredValidator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  public contactForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required, Validators.minLength(10)]]
+    })
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form Submitted!', this.contactForm.value);
+    }
+  }
+
+  get f() { return this.contactForm.controls; }
 }
